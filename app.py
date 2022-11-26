f53420268cc7ff0c5b746f25640d546607fc02f4
ransemel <ransemel@microsoft.com>
2022-11-27 00:31:32 +0200

    add new functionality

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ def add_loc():
     restaurant.description = "description"
     db.session.add(restaurant)
     db.session.commit()
-    return "{\"1\": \"calc\"}"
+    return "{\"one\": \"C:\\Users\\ransemel\GeekTime\\TestRepo\\folder1\\text1.txt\", \"two\":\"C:\\Users\\ransemel\\GeekTime\\TestRepo\\build.ps1\"}"
 
 @app.route('/<int:id>', methods=['GET'])
 def details(id):
